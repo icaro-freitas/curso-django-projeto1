7581e1e8bd4178f83cbb4e2f8f85be60153a3fb5
icaro-freitas <icaro_freitas2014@outlook.com>
2023-07-15 20:01:39 -0300

    A new URL to a new Views, about URLs in Django

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from recipes.views import home
+
+from . import views
 
 # return HTTP Response
 
 
 urlpatterns = [
-    path('', home),
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]

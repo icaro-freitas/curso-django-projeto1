90c08ca0f78af8a7b5fe69cba86891b3a0ca06e8
icaro-freitas <icaro_freitas2014@outlook.com>
2023-08-05 14:26:36 -0300

    Returning the correct template with TDD

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -125,3 +125,7 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_search_uses_correct_view_finction(self):
         resolved = resolve(reverse('recipes:search'))
         self.assertIs(resolved.func, views.search)
+
+    def test_recipe_search_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:search'))
+        self.assertTemplateUsed(response, 'recipes/pages/search.html')

950c3e1a01986bb1a922a2c1bae74b51072ed453
icaro-freitas <icaro_freitas2014@outlook.com>
2023-08-16 15:49:55 -0300

    environment variables wiht python-dotenv

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
-from django.core.paginator import Paginator
+
+import os
+
 from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 
 from .models import Recipe
 
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 # Create your views here.
 
 
@@ -15,19 +18,7 @@ def home(request):
         is_published=True,
     ).order_by('-id')
 
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:
-        current_page = 1
-
-    paginator = Paginator(recipes, 9)
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj, 'pagination_range': pagination_range
@@ -40,8 +31,11 @@ def category(request, category_id):
         is_published=True
     ).order_by('-id'))
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name }  - Category | '
     })
 
@@ -69,8 +63,11 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" | ',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj, 'pagination_range': pagination_range,
+        'aditional_url_query': f'&q={search_term}',
     })
--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -10,7 +10,7 @@ class PaginationTest(TestCase):
             qty_pages=4,
             current_page=1,
         )
-        self.assertEqual([1, 2, 3, 4], pagination)
+        self.assertEqual([1, 2, 3, 4], pagination['pagination'])
 
     def test_first_range_is_static_if_current_page_is_less_than_middle_page(self):  # noqa e501
         # Current page = 1 - Qty Page = 2 - Middle Page = 2

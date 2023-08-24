2af7b04271aba9141b5bc1cd52412a2734aa7828
icaro-freitas <icaro_freitas2014@outlook.com>
2023-08-24 15:09:09 -0300

    Testing authors Form help_text

--- authors/forms.py
+++ authors/forms.py
@@ -18,8 +18,10 @@ def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
     if not regex.match(password):
-        raise ValidationError('Password mut have at least one uppercase letter, '
-                              'one lowercase letter and one number. The length should be '
+        raise ValidationError('Password mut have at least'
+                              ' one uppercase letter, '
+                              'one lowercase letter and one number.'
+                              ' The length should be '
                               'at least 8 characters.', code='invalid')
 
 

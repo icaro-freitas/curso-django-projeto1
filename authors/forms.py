d63b3a8b9532129eba4d59d78cdf811398e27ccb
icaro-freitas <icaro_freitas2014@outlook.com>
2023-08-28 12:46:27 -0300

    Testing min and max length of a  field

--- authors/forms.py
+++ authors/forms.py
@@ -35,6 +35,19 @@ class RegisterForm(forms.ModelForm):
         add_placeholer(self.fields['password'], 'Type your password')
         add_placeholer(self.fields['password2'], 'Repeat your password')
 
+    username = forms.CharField(
+        label='Username',
+        help_text='Username must have letters, '
+        'numbers or one of those @.+-_. '
+        'The length should be between 4 and 150 characters.',
+        error_messages={
+            'required': 'This field must not be empty',
+            'min_length': 'Username must have at least 4 characters.',
+            'max_length': 'Username must have less than 150 characters.',
+        },
+        min_length=4, max_length=150,
+    )
+
     first_name = forms.CharField(
         error_messages={'required': 'Write your first name'},
         label='First name'
@@ -80,19 +93,6 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-
-        labels = {
-            'username': 'Username',
-            'first_name': 'First name',
-            'last_name': 'Last name',
-            'email': 'E-mail',
-        }
-
-        error_messages = {
-            'username': {
-                'required': 'This field must not be empty',
-            }
-        }
 
     def clean(self):
         cleaned_data = super().clean()
--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -22,8 +22,9 @@ class AuthorRegisterFormUnitest(TestCase):
         self.assertEqual(current_placeholder, placeholder)
 
     @parameterized.expand([
-        ('username', 'Obrigatório. 150 caracteres ou menos.'
-         ' Letras, números e @/./+/-/_ apenas.'),
+        ('username', 'Username must have letters, '
+         'numbers or one of those @.+-_. '
+         'The length should be between 4 and 150 characters.'),
         ('email', 'The e-mail must be valid'),
         ('password', ('Password mut have at least one uppercase letter, '
                       'one lowercase letter and one number.'
@@ -76,3 +77,31 @@ class AuthorRegisterFormIntegrationTest(DjangoTestCase):
         response = self.client.post(url, data=self.form_data, follow=True)
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get(field))
+
+    def test_username_min_length_should_be_4(self):
+        self.form_data['username'] = 'joa'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Username must have at least 4 characters.'
+        self.assertIn(msg, response.content.decode('utf-8'))
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+
+    def test_username_max_length_should_be_150(self):
+        self.form_data['username'] = 'A' * 151
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = 'Username must have less than 150 characters.'
+        self.assertIn(msg, response.context['form'].errors.get('username'))
+        self.assertIn(msg, response.content.decode('utf-8'))
+
+    def test_password_field_have_lower_upper_case_letters_and_numbers(self):
+        self.form_data['password'] = 'abc123'
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        msg = ('Password mut have at least'
+               ' one uppercase letter, '
+               'one lowercase letter and one number.'
+               ' The length should be '
+               'at least 8 characters.')
+        self.assertIn(msg, response.context['form'].errors.get('password'))
+        self.assertIn(msg, response.content.decode('utf-8'))

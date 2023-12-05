29d978508e5d44c5141a3667446574fb90632979
icaro-freitas <icaro_freitas2014@outlook.com>
2023-12-05 18:03:55 -0300

    Fixing N + 1 Queries with select_related

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,6 @@
 
 import os
-from typing import Any
 
-from django import http
-from django.db import models
 from django.db.models import Q
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
@@ -29,7 +26,7 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
-
+        qs = qs.select_related('author', 'category')
         return qs
 
     def get_context_data(self, *args, **kwargs):

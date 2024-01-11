d45be8b3b257cc2d92d0a492728dd3b7a41eea30
icaro-freitas <icaro_freitas2014@outlook.com>
2024-01-11 14:57:30 -0300

    Recipe published and category filters test

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -7,9 +7,32 @@ from recipes.tests.test_recipe_base import RecipeMixin
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_recipe_api_list_returns_status_code_200(self):
-        api_url = reverse('recipes:recipes-api-list')
+    def get_recipe_reverse_url(self, reverse_result=None):
+        api_url = reverse_result or reverse('recipes:recipes-api-list')
+        return api_url
+
+    def get_recipe_api_list(self, reverse_result=None):
+        api_url = self.get_recipe_reverse_url(reverse_result)
         response = self.client.get(api_url)
+        return response
+
+    def get_jwt_access_token(self):
+        userdata = {
+            'username': 'user',
+            'password': 'password',
+        }
+        self.make_author(
+            username=userdata.get('username'),
+            password=userdata.get('password')
+        )
+        response = self.client.post(
+            self.reverse('recipes:token_obtain_pair'),
+            data={**userdata}
+        )
+        return response.data.get('access')
+
+    def test_recipe_api_list_returns_status_code_200(self):
+        response = self.get_recipe_api_list()
         self.assertEqual(
             response.status_code,
             200
@@ -29,3 +52,56 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             recipes_wanted_number,
             recipes_loaded_quantity
         )
+
+    def test_recipe_api_list_do_not_show_not_published_recipes(self):
+        recipes = self.make_recipe_in_batch(qtd=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            len(response.data.get('results')),
+            1
+        )
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
+    def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # Creates categories
+        category_wanted = self.make_category(name='WANTED_CATEGORY')
+        category_not_wanted = self.make_category(name='NOT_WANTED_CATEGORY')
+
+        # Creates 10 recipes
+        recipes = self.make_recipe_in_batch(qtd=10)
+
+        # Changed all recipes to the wanted category
+        for recipe in recipes:
+            recipe.category = category_wanted
+            recipe.save()
+
+        # Change one recipe to the NOT wanted category
+        # As a result, this recipe should NOT show in the page
+        recipes[0].category = category_not_wanted
+        recipes[0].save()
+
+        # Action: get recipes by wanted category_id
+        api_url = reverse('recipes:recipes-api-list') + \
+            f'?category_id={category_wanted.id}'
+        response = self.get_recipe_api_list(reverse_result=api_url)
+
+        # We should only see recipes from the wanted category
+        self.assertEqual(
+            len(response.data.get('results')),
+            9
+        )
+
+    def test_recipe_api_list_user_must_send_jwt_token_to_create(self):
+        api_url = self.get_recipe_reverse_url()
+        response = self.client.post(api_url)
+        self.assertEqual(
+            response.status_code,
+            401
+        )
+
+    def test_jwt_login(self):
+        self.get_jwt_access_token()
